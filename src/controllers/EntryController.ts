8aa428b4d392bf955703096bcf5ae78967d28885
0xpranv <pranav@greendreamearth.org>
2024-08-29 00:00:41 +0530

    chore: Update EntryController response JSON keys

--- src/controllers/EntryController.ts
+++ src/controllers/EntryController.ts
@@ -9,7 +9,7 @@ export const createEntry = async (req: Request, res: Response) => {
       data: newEntry,
     });
     console.log(response);
-    res.status(200).json(response);
+    res.status(200).json({ response: response });
   } catch (e: any) {
     res.status(500).json({ msg: "couldn't create a new Entry : " + e.message });
   }
@@ -37,7 +37,7 @@ export async function getAllEntries(req: Request, res: Response) {
   try {
     const response = await prisma.entry.findMany();
     console.log(response);
-    res.status(200).json(response);
+    res.status(200).json({ response: response });
   } catch (e: any) {
     res.status(500).json({ msg: "couldn't fetch all Entry : " + e.message });
   }
@@ -52,7 +52,7 @@ export async function deleteEntry(req: Request, res: Response) {
       },
     });
     console.log(response);
-    res.status(200).json({ "Entry deleted : ": response });
+    res.status(200).json({ msg: "entry deleted", response: response });
   } catch (e: any) {
     res.status(500).json({ msg: "couldn't delete Entry : " + e.message });
   }
